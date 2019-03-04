4361d741fbd0c1b012ba868ec714cb7a6b15d06e
Wassim <wawa.h8@gmail.com>
2019-03-04 16:02:33 +0100

    Using ArrayBuffer to read file

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function decypher(){
   var file = document.getElementById("file").files[0];
   if (file) {
       var reader = new FileReader();
-      reader.readAsText(file, "UTF-8");
+      reader.readAsArrayBuffer(file);
       reader.onload = function (evt) {
           var cypher = evt.target.result;
           document.getElementById("cypher").innerHTML = cypher;
           const digest = crypto.subtle.digest('SHA-256', cypher);
           console.log(digest)
-          document.getElementById("digest").innerHTML = digest;
+          document.getElementById("digest").innerHTML = digest.toString();
       }
       reader.onerror = function (evt) {
           document.getElementById("cypher").innerHTML = "error reading file";
