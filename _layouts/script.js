511366ce7d6222625718b8b76cc5cbe3c8db0a87
Wassim <wawa.h8@gmail.com>
2019-03-04 15:52:18 +0100

    decypher button

--- _layouts/script.js
+++ _layouts/script.js
@@ -1,14 +1,16 @@
-var file = document.getElementById("file").files[0];
-if (file) {
-    var reader = new FileReader();
-    reader.readAsText(file, "UTF-8");
-    reader.onload = function (evt) {
-        var cypher = evt.target.result;
-        document.getElementById("cypher").innerHTML = cypher;
-        const digest = crypto.subtle.digest('SHA-256', cypher);
-        document.getElementById("digest").innerHTML = digest;
-    }
-    reader.onerror = function (evt) {
-        document.getElementById("cypher").innerHTML = "error reading file";
-    }
+function decypher(){
+  var file = document.getElementById("file").files[0];
+  if (file) {
+      var reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = function (evt) {
+          var cypher = evt.target.result;
+          document.getElementById("cypher").innerHTML = cypher;
+          const digest = crypto.subtle.digest('SHA-256', cypher);
+          document.getElementById("digest").innerHTML = digest;
+      }
+      reader.onerror = function (evt) {
+          document.getElementById("cypher").innerHTML = "error reading file";
+      }
+  }
 }
